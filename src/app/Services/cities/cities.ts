11fd7bf73f872847cdc755778d1a0c865c3757c7
EwenLM <ewenqglm@gmail.com>
2026-03-03 09:57:05 +0100

    add pipe on cities loop

--- src/app/Services/cities/cities.ts
+++ src/app/Services/cities/cities.ts
@@ -8,6 +8,10 @@ export class Cities {
   cities: City[] = [
     { name: 'Lyon', attribut: 'Eteint' },
     { name: 'London', attribut: 'Allume' },
+    { name: 'Paris', attribut: 'Eteint' },
+    { name: 'New York', attribut: 'Allume' },
+    { name: 'Tokyo', attribut: 'Eteint' },
+    { name: 'Berlin', attribut: 'Allume' },
   ];
 
   //Methode pour changer l'attribut d'une ville
